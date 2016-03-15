2a8b6a5e48c0c83146c4056281e1279aa61fc87f
Ming Wen and Parker Catalano <parkercatalano@gmail.com>
2016-03-14 21:02:39 -0700

    Add click listener, form submit listener, and address info inside .contact.last click listener

--- js/scripts.js
+++ js/scripts.js
@@ -12,6 +12,23 @@ function Address(street, city, state) {
 
 // user interface logic
 $(document).ready(function() {
+
+  $("#add-address").click(function() {
+  $("#new-addresses").append('<div class="new-address">' +
+                               '<div class="form-group">' +
+                                 '<label for="new-street">Street</label>' +
+                                 '<input type="text" class="form-control new-street">' +
+                               '</div>' +
+                               '<div class="form-group">' +
+                                 '<label for="new-city">City</label>' +
+                                 '<input type="text" class="form-control new-city">' +
+                               '</div>' +
+                               '<div class="form-group">' +
+                                 '<label for="new-state">State</label>' +
+                                 '<input type="text" class="form-control new-state">' +
+                               '</div>' +
+                             '</div>');
+});
   $("form#new-contact").submit(function(event) {
     event.preventDefault();
 
@@ -19,6 +36,14 @@ $(document).ready(function() {
     var inputtedLastName = $("input#new-last-name").val();
 
     var newContact = new Contact(inputtedFirstName, inputtedLastName);
+
+    $(".new-address").each(function() {
+   var inputtedStreet = $(this).find("input.new-street").val();
+   var inputtedCity = $(this).find("input.new-city").val();
+   var inputtedState = $(this).find("input.new-state").val();
+   var newAddress = new Address(inputtedStreet, inputtedCity, inputtedState)
+   newContact.addresses.push(newAddress)
+ });
 
     $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + " " + newContact.lastName + "</span></li>");
 
@@ -28,6 +53,10 @@ $(document).ready(function() {
       $("#show-contact h2").text(newContact.firstName);
       $(".first-name").text(newContact.firstName);
       $(".last-name").text(newContact.lastName);
+      $("ul#addresses").text("");
+newContact.addresses.forEach(function(address) {
+  $("ul#addresses").append("<li>" + address.street + ", " + address.city + " " + address.state + "</li>");
+});
     });
 
     $("input#new-first-name").val("");
