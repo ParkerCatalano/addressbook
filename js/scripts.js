ccb9f089805be89a4f9eed92fcae3f6948c0298f
Ming Wen and Parker Catalano <mwen82@gmail.com>
2016-03-21 18:48:00 -0700

    create reset fields function for inputted values + prototype methods for fullAddress and fullName

--- js/scripts.js
+++ js/scripts.js
@@ -10,6 +10,22 @@ function Address(street, city, state) {
   this.state = state;
 }
 
+Address.prototype.fullAddress = function() {
+  return this.street + ", " + this.city + ", " + this.state;
+}
+
+Contact.prototype.fullName = function() {
+  return this.firstName + ", " + this.lastName;
+}
+
+function resetFields() {
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+    $("input.new-street").val("");
+    $("input.new-city").val("");
+    $("input.new-state").val("");
+}
+
 // user interface logic
 $(document).ready(function() {
 
@@ -45,7 +61,7 @@ $(document).ready(function() {
    newContact.addresses.push(newAddress)
  });
 
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + " " + newContact.lastName + "</span></li>");
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
 
 
     $(".contact").last().click(function() {
@@ -55,12 +71,11 @@ $(document).ready(function() {
       $(".last-name").text(newContact.lastName);
       $("ul#addresses").text("");
 newContact.addresses.forEach(function(address) {
-  $("ul#addresses").append("<li>" + address.street + ", " + address.city + " " + address.state + "</li>");
+  $("ul#addresses").append("<li>" + address.fullAddress() + "</li>");
 });
     });
 
-    $("input#new-first-name").val("");
-    $("input#new-last-name").val("");
+    resetFields();
 
   });
 });
